38d129ef21dd5f8099f93a3f343890f8d8fbd120
Tommy <treif23@github.com>
2018-06-21 15:14:44 -0700

    update 6/21/18

--- src/js/containers/MovieDetailContainer.jsx
+++ src/js/containers/MovieDetailContainer.jsx
@@ -9,8 +9,23 @@ class MovieDetailContainer extends React.Component {
     return (
       <div>
         <h1>Movie Detail Container</h1>
-
         <p>Viewing movie {this.props.match.params.id}</p>
+        <div className='container'>
+          <div id='info' className='row'>
+
+            <div className='col-5'>
+              <div> IMAGE!! <div>
+              </div>
+                <div id='movie' className='col-5'>
+                  <p id='title1'>MOVIE TITLE</p>
+                  <p>(MOVIE YEAR)</p>
+                  <p>MOVIE DESCRIPTION</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     )
   }
